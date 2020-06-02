7792cc1bae592ff3c75558f9a14795df16c8f3e2
Lucas Tiago <lucastiago45@gmail.com>
2020-06-01 22:08:57 -0300

    atualizando avaliação

--- app.js
+++ app.js
@@ -24,21 +24,6 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.use((req, res, next) => {
-//     const error = new Error('not found');
-//     error.status = 404;
-//     next(error);
-// })
-
-// app.use((error, req, res, next) => {
-//     res.status(error.status || 500);
-//     res.json({
-//         error: {
-//             messagem: error.messagem
-//         }
-//     })
-// })
-
 const rotaUsuario = require('./api/route/usuario/usuario.route')
 
 app.use('/usuario', rotaUsuario);
